293cf11cb753c9a72ddf3fbdcbdcfbd5f5618e97
usuario001 <patinogutierrez@icloud.com>
2025-03-20 19:08:00 -0600

    Testing some bindings and events

--- ko-vanila/main.js
+++ ko-vanila/main.js
@@ -1,6 +1,15 @@
 // Definir el ViewModel
 function AppViewModel() {
     this.nombre = ko.observable('');
+    this.nombreDos = ko.observable('');
+    this.numerico = ko.observable(0);
+    this.numeroDos = ko.observable();
+
+}
+
+function updateItemQty() {
+    var currentQty = this.numeroDos();
+    console.log(currentQty);
 }
 
 // Aplicar los bindings
